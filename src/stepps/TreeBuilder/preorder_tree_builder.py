77be069fc8a4f214e167ef9c40feed63c709525a
TrinayMitra <trinay.mitra@vaastav.tech>
2026-09-07 13:44:02 +0530

    feat: add preorder tree builder from Sequence
    - Added the PreOrderTreeBuilder interface.
    - Added the PreOrderTreeBuilderImpl implementation..
    - Added tests for empty, single-element, odd-length, even-length, and long sequences and structure validation tests.

---
+++ src/stepps/TreeBuilder/preorder_tree_builder.py
@@ -0,0 +1,8 @@
+from stepps.TreeBuilder.tree_builder import TreeBuilder
+
+
+class PreOrderTreeBuilder[T](TreeBuilder[T]):
+    """
+    Define the interface for building a binary tree from a sequence
+    using preorder traversal.
+    """
